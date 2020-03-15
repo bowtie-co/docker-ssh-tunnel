3cc6ab995bd3622fffd30cde8eff0224d516032b
Charlie McClung <charlie@cmr1.com>
2020-03-15 15:06:36 -0600

    Updates for ssh tunnel usage

--- bin/ping.sh
+++ bin/ping.sh
@@ -2,33 +2,96 @@
 
 set -e
 
-SSH_USER=${SSH_USER:-charlie}
-SSH_HOST=${SSH_HOST:-ssh-host}
+# COLORS! :)
+red='\033[0;31m'
+cyan='\033[0;36m'
+blue='\033[0;34m'
+yellow='\033[0;33m'
+nocolor='\033[0m'
+
+error() {
+  prefix="[ERROR] "
+  echo -e "${red}${prefix}${1}${nocolor}"
+}
+
+warn() {
+  prefix="[WARNING] "
+  echo -e "${yellow}${prefix}${1}${nocolor}"
+}
+
+debug() {
+  if [[ "$VERBOSE" != "" ]]; then
+    prefix="[DEBUG] "
+    echo -e "${cyan}${prefix}${1}${nocolor}"
+  fi
+}
+
+has_env_var() {
+  env | grep "$1" > /dev/null 2>&1
+  return $?
+}
+
+missing_env_var() {
+  if has_env_var $1; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+load_env_file() {
+  if [[ ! -f $1 ]]; then
+    warn "Missing ENV file: $1"
+  else
+    ENV_VARS=$(cat $1)
+
+    export $(echo $ENV_VARS | xargs)
+  fi
+}
+
+if [[ -f ".env" ]]; then
+  load_env_file ".env"
+fi
+
+if [[ "$APP_ENV" != "" ]]; then
+  load_env_file ".env.$APP_ENV"
+fi
+
 SSH_PORT=${SSH_PORT:-22}
-
 LOCAL_HOST=${LOCAL_HOST:-0.0.0.0}
-LOCAL_PORT=${LOCAL_PORT:-3306}
-
-REMOTE_HOST=${REMOTE_HOST:-remote-host}
-REMOTE_PORT=${REMOTE_PORT:-3306}
-
 PING_HOST=$LOCAL_HOST
 PING_PORT=$LOCAL_PORT
 PING_WAIT=${PING_WAIT:-30}
+
+REQUIRED_ENV_VARS="
+SSH_USER
+SSH_HOST
+LOCAL_HOST
+LOCAL_PORT
+REMOTE_HOST
+REMOTE_PORT
+"
+
+for v in $REQUIRED_ENV_VARS; do
+  if missing_env_var "$v"; then
+    error "Missing required ENV VAR: '$v'"
+  fi
+done
 
 if [[ "$PING_HOST" == "0.0.0.0" ]]; then
   PING_HOST=localhost
 fi
 
-# TODO: Verify / validate variables before attempting?
+cmd="nc -zv -w $PING_WAIT $PING_HOST $PING_PORT"
 
-cmd="nc -zv -w $PING_WAIT $PING_HOST $LOCAL_PORT"
-
-echo "SSH Tunnel Ping: '$cmd'"
+debug "SSH Tunnel Ping: '$cmd'"
 
 out=$($cmd)
 
-echo "res: $? | out: $out"
+if [[ "$?" != "0" ]]; then
+  error "Failed to ping tunnel - $PING_HOST:$PING_PORT"
+  exit 1
+fi
 
 sleep $PING_WAIT
 
--- bin/tunnel.sh
+++ bin/tunnel.sh
@@ -2,23 +2,88 @@
 
 set -e
 
-SSH_USER=${SSH_USER:-charlie}
-SSH_HOST=${SSH_HOST:-ssh-host}
+# COLORS! :)
+red='\033[0;31m'
+cyan='\033[0;36m'
+blue='\033[0;34m'
+yellow='\033[0;33m'
+nocolor='\033[0m'
+
+error() {
+  prefix=${2:-"[ERROR] "}
+  echo -e "${red}${prefix}${1}${nocolor}"
+}
+
+warn() {
+  prefix=${2:-"[WARNING] "}
+  echo -e "${yellow}${prefix}${1}${nocolor}"
+}
+
+debug() {
+  if [[ "$VERBOSE" != "" ]]; then
+    prefix=${2:-"[DEBUG] "}
+    echo -e "${cyan}${prefix}${1}${nocolor}"
+  fi
+}
+
+has_env_var() {
+  env | grep "$1" > /dev/null 2>&1
+  return $?
+}
+
+missing_env_var() {
+  if has_env_var $1; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+load_env_file() {
+  if [[ ! -f $1 ]]; then
+    warn "Missing ENV file: $1"
+  else
+    ENV_VARS=$(cat $1)
+
+    export $(echo $ENV_VARS | xargs)
+  fi
+}
+
+if [[ -f ".env" ]]; then
+  load_env_file ".env"
+fi
+
+if [[ "$APP_ENV" != "" ]]; then
+  load_env_file ".env.$APP_ENV"
+fi
+
 SSH_PORT=${SSH_PORT:-22}
-
 LOCAL_HOST=${LOCAL_HOST:-0.0.0.0}
-LOCAL_PORT=${LOCAL_PORT:-3306}
 
-REMOTE_HOST=${REMOTE_HOST:-remote-host}
-REMOTE_PORT=${REMOTE_PORT:-3306}
-REMOTE_WAIT=${REMOTE_WAIT:-10}
+REQUIRED_ENV_VARS="
+SSH_USER
+SSH_HOST
+SSH_PORT
+LOCAL_HOST
+LOCAL_PORT
+REMOTE_HOST
+REMOTE_PORT
+"
 
-# TODO: Verify / validate variables before attempting?
+for v in $REQUIRED_ENV_VARS; do
+  if missing_env_var "$v"; then
+    error "Missing required ENV VAR: '$v'"
+  fi
+done
 
-cmd="ssh -f -4 $SSH_USER@$SSH_HOST -L $LOCAL_HOST:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT -N"
+cmd="ssh -f -4 -p $SSH_PORT $SSH_USER@$SSH_HOST -L $LOCAL_HOST:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT -N"
 
-echo "SSH Tunnel: '$cmd'"
+debug "SSH Tunnel: '$cmd'"
 
 $($cmd)
+
+warn "$(date)" "[*] "
+warn " Tunnel - $LOCAL_HOST:$LOCAL_PORT => $REMOTE_HOST:$REMOTE_PORT" "[*] "
+warn "Via SSH - $SSH_USER@$SSH_HOST:$SSH_PORT" "[*] "
 
 exec "$@"
